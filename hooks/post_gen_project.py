abf2478246cdd411d4f3585fcb22e9c684554a35
Jimmy Girardet <test@ma.fr>
2018-01-29 17:36:35 +0100

    post gen

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,9 +5,10 @@ from __future__ import print_function
 
 import os
 import subprocess
-
+import pathlib
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
+project_dir = pathlib.Path(PROJECT_DIRECTORY)
 
 
 def remove_file(filepath):
@@ -21,6 +22,10 @@ if __name__ == '__main__':
     print("Creating git repository (needed for PBR to fully work)")
     subprocess.check_call(["git", "init", "."])
 
+    # Add a dir for sphinx
+    staticdir = project_dir / 'docs/_static/'
+    staticdir.mkdir(exist_ok=True)
+
     if '{{ cookiecutter.create_developer_env_after_scapfolding }}' == 'y':
         print("Setting up a virtual environment")
         pyversion = "--three"
@@ -30,12 +35,13 @@ if __name__ == '__main__':
         subprocess.check_call(["pipenv", "install", "--dev"])
 
         print("Initial build...")
-        venv = subprocess.check_output(["pipenv", "--venv"]).strip().decode('utf8')
+        venv = subprocess.check_output(["pipenv",
+                                        "--venv"]).strip().decode('utf8')
         # Cannot use directly `pipenv run`, it requires a TTY, and the
         # --no-interactive options is not available on every version
-        subprocess.check_call([os.path.join(venv,
-                                            "bin",
-                                            "python"), "setup.py", "sdist"])
+        subprocess.check_call(
+            [os.path.join(venv, "bin", "python"), "setup.py", "sdist"])
 
         print("Developer environment created. Activate with:")
         print("  pipenv shell")
+        print(PROJECT_DIRECTORY)
