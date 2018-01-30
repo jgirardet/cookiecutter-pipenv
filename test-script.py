92cc95da2e638dc5e7a49d9ec4a0f1d62b2007ab
jgirardet <moi@moi>
2018-01-30 19:00:45 +0100

    remove auto scap

--- test-script.py
+++ test-script.py
@@ -8,5 +8,6 @@ cookiecutter(
     'https://github.com/jgirardet/python-library-cookiecutter',
     no_input=True,
     extra_context={
-        "python_version": sys.version[:3]
+        "python_version": sys.version[:3],
+        "create_developer_env_after_scapfolding": "n",
     })
