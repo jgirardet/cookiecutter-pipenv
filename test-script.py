a9677f38ed85fc23baa45289e9488a0dadf9dc48
jgirardet <ijkl@tuta.io>
2018-01-30 22:50:19 +0100

    Update test-script.py

--- test-script.py
+++ test-script.py
@@ -7,9 +7,8 @@ just launch a cookie cookiecutter
 cookiecutter(
     'https://github.com/jgirardet/python-library-cookiecutter',
     no_input=True,
+    #install specifique version en travis and pipenv bug under travis, make all does the job next
     extra_context={
-        "python_version":
-        sys.version[:3],  # install specifique version en travis
-        "create_developer_env_after_scapfolding":
-        "n",  #  pipenv bug under travis, make all does the job next
+        "python_version": sys.version[:3],
+        "create_developer_env_after_scapfolding": "n",
     })
