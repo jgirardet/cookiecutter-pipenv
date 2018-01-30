9b322d3d84c668f6c9677d47fc951c3171c30296
jgirardet <moi@moi>
2018-01-30 17:32:08 +0100

    test

--- {{cookiecutter.project_slug}}/test-scipt.py
+++
@@ -1,9 +0,0 @@
-from cookiecutter.main import cookiecutter
-import sys
-
-cookiecutter(
-    'https://github.com/jgirardet/python-library-cookiecutter',
-    no_input=True,
-    extra_context={
-        "python_version": sys.version[:3]
-    })
