074984f33a448e32bd76405b6f3da29ad6039b02
jgirardet <moi@moi>
2018-02-04 08:51:38 +0100

    travis

--- test-script.py
+++ test-script.py
@@ -3,6 +3,7 @@ import sys
 from cookiecutter.main import cookiecutter
 """
 just launch a cookie cookiecutter
+dont-t change my_project_name
 """
 
 cookiecutter(
@@ -13,3 +14,13 @@ cookiecutter(
         "python_version": sys.version[:3],
         "create_developer_env_after_scapfolding": "n",
     })
+"""
+.env has to be created at start of project
+
+"""
+import pathlib
+chemin = pathlib.Path("my_project_name/.env")
+chemin.touch()
+chemin.write_text(
+    "DB_ENGINE=django.db.backends.sqlite3\nDB_NAME=db.local\nSECRET_KEY=mokmokmok"
+)
